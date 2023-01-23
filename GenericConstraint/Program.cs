a7d66ce565b7ac1330e65e0c8a99c409e5fbd8ac
Erismar Oliveira <erismarpro@hotmail.com>
2023-01-22 23:31:07 -0300

    Initial commit

---
+++ GenericConstraint/Program.cs
@@ -0,0 +1,28 @@
+﻿var handler = new Handler<Request, Response>();
+var result = handler.Handle(null);
+if (result.Message is null)
+{
+}
+
+public class Handler<TRequest, TResponse>
+    where TRequest : IRequest
+    where TResponse : IResponse
+{
+    public TResponse Handle(TRequest request)
+    {
+        return default;
+    }
+}
+
+public interface IRequest { }
+public interface IResponse { }
+
+public class Request : IRequest
+{
+    public string Email { get; set; } = string.Empty;
+}
+
+public class Response : IResponse
+{
+    public string Message { get; set; } = string.Empty;
+}
